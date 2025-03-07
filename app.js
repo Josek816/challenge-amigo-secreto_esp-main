9ff173e4263e71a13225ad5e0a72bfc5ce90cefd
José <josek816@outlook.com>
2025-03-07 17:00:00 -0500

    Primer commit_Array para almacenar los nombres de amigos

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// Array para almacenar los nombres de amigos
+let amigos = [];
+
+// Función para agregar un amigo
+function agregarAmigo() {
+    // Obtener el valor del campo de texto
+    let nombreAmigo = document.getElementById("amigo").value;
